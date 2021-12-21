e16b343a5198850cf254077f735db3f4bc8edff6
Angélica Ciriaco <angelicaciriaco64@gmail.com>
2021-12-21 14:30:44 -0300

    atualizando o README

---
+++ site-teste/index.js
@@ -0,0 +1,8 @@
+function longin(){
+    alert("chrgou aq");
+    let email = document.getElementById("email").valeu;
+    let senha = document.getElementById("senha").valeu;
+    alert(email);
+    alert(senha);
+
+}
